0da670c4f79206e48d9aeda7ae8068bf999902c8
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-03-27 12:00:01 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (90.68%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
-    void solve(string digits, vector<string>& ans, int index, string temp, vector<string>& map){
+    void init(vector<string>& mp){
+        mp={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    }
+    void solve(int index, string& digits, vector<string>& mp, vector<string>& ans, string& temp){
         if(index==digits.size()){
             ans.push_back(temp);
             return;
         }
-        int digit=digits[index]-'0';
-        string key=map[digit];
-        for(int i=0;i<key.size();i++){
-            temp.push_back(key[i]);
-            solve(digits,ans,index+1,temp,map);
+        
+        string comb=mp[digits[index]-'0'];
+
+        for(auto i: comb){
+            temp.push_back(i);
+            solve(index+1,digits,mp,ans,temp);
             temp.pop_back();
         }
     }
+
     vector<string> letterCombinations(string digits) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
+        vector<string>mp;
+        init(mp);
         vector<string> ans;
-        if(digits.size()==0)
+        if(digits=="")
             return ans;
-       vector<string> num ={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        solve(digits,ans,0,"",num);
+        string temp="";
+        solve(0,digits,mp,ans,temp);
         return ans;
     }
 };
-
