b6b543146109fd5440d253a12b027505f2c88cd4
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-04-19 11:23:57 +0530

    Time: 18 ms (43.85%), Space: 18.3 MB (26.36%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int,vector<int>> mp;
+        vector<int> is_a_prerequisite(numCourses,0);
+        vector<int> ans;
+        for(auto i:prerequisites){
+            mp[i[0]].push_back(i[1]);
+            is_a_prerequisite[i[1]]++;
+        }
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(is_a_prerequisite[i]==0)
+                q.push(i);
+        }
+        while(!q.empty()){
+            int temp=q.front();
+            q.pop();
+            ans.push_back(temp);
+            for(auto i:mp[temp]){
+                is_a_prerequisite[i]--;
+                if(is_a_prerequisite[i]==0)
+                    q.push(i);
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans.size()!=numCourses)
+            return {};
+        return ans;
+    }
+};
