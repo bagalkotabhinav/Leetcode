17cf2802c6cf24384197c29a550980cfb2761199
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-05-19 22:18:17 +0530

    Time: 3 ms (80.03%), Space: 16.2 MB (60.73%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    int ans(vector<int>& cost, int n,vector<int>& dp){
-        //BASE CASE
-        if(n==0)
-            return cost[0];
-        if(n==1)
-            return cost[1];
-        //TO REMOVE OVERLAPPING COMPUTATIONS
-        if(dp[n]!=-1)
-            return dp[n];
-        //MEMOISATION: STORING ANSWERS
-        dp[n]=cost[n]+min(ans(cost,n-1,dp),ans(cost,n-2,dp));
-        return dp[n];
-    }
-    int ans2(vector<int>& cost, int n){
-        vector<int> dp(n+1);
-        dp[0]=cost[0];
-        dp[1]=cost[1];
-        for(int i=2;i<=n-1;i++){
-            dp[i]=cost[i]+min(dp[i-1],dp[i-2]);
-        }
-        return(min(dp[n-2],dp[n-1]));
-    }
     int minCostClimbingStairs(vector<int>& cost) {
         int n=cost.size();
-        vector<int> dp(n,-1);
-        int a=min(ans(cost,n-1,dp),ans(cost,n-2,dp));
-        int b=ans2(cost,n);
-        return b;
+        int first=0;
+        int second=0;
+        int third;
+        for(int i=2;i<=n;i++){
+            third=min(first+cost[i-2],second+cost[i-1]);
+            first=second;
+            second=third;
+        }
+        return third;
     }
 };
 
+//dp[i]=cost to reach dp[i-2]+cost[i-2] or cost to reach dp[i-1]+cost[i-1]
+//dp[0]=0 dp[1]=0 dp[i]=min(dp[i-1]+cost[i-1],dp[i-2]+cost[i-2])
