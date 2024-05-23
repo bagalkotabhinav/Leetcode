f0909cc3e624c6a929cb813d78ce93138f0954bc
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-05-23 09:11:05 +0530

    Time: 20 ms (82.96%), Space: 16.1 MB (67.61%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount, vector<int>& dp){
-
-        if(amount==0)
-            return 0;
-
-        if(amount<0)
-            return INT_MAX;
-
-        if(dp[amount]!=-1)
-            return dp[amount];
-
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++){
-            int x=solve(coins,amount-coins[i],dp);
-            if(x!=INT_MAX){
-                mini=min(mini,x+1);
-            }
-        }
-        dp[amount]=mini;
-        return mini;
-    }
-    int solve2(vector<int>& coins, int amount){
-        vector<int> dp(amount+1,INT_MAX);
-        dp[0]=0;
-        int mini=INT_MAX;
-        for(int i=1;i<=amount;i++){
-            for(int j=0;j<coins.size();j++){
-                if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX)
-                    dp[i]=min(dp[i],1+dp[i-coins[j]]);
-            }
-        }
-        if(dp[amount]==INT_MAX)
-            return -1;
-        return dp[amount];
-    }
     int coinChange(vector<int>& coins, int amount) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        vector<int> dp(amount+1,-1);
-        int ans=solve(coins,amount,dp);
-        if(ans==INT_MAX)
-            return -1;
-        return ans;
+//DP ARRAY: dp[i]= min number of coins from given coin array to amount to i
+// dp[3]=min coins required that make up 3 from given denomination
+        vector<int> dp(amount+1,INT_MAX/2);
+        dp[0]=0;
+
+// for each value 1 to amount, try to divide it thru all given denominations
+// amt=11: 1 thru all 1,2,5: 2 thru all 1,2,5...... 11 thru all 1,2,5
+        for(int value=1;value<=amount;value++){
+            for(auto denomination: coins){
+                if(value-denomination>=0){
+                    dp[value]=min(dp[value],1+dp[value-denomination]);
+                }
+            }
+        }
+        return dp[amount]!=INT_MAX/2?dp[amount]:-1;
     }
 };
