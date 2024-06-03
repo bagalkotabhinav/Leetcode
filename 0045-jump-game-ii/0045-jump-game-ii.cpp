ba77d636db3a9ca45f54b1c6651636809bfbcb07
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-06-03 14:07:40 +0530

    Time: 12 ms (59.65%), Space: 18.9 MB (87.23%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,37 +1,17 @@
-/*
-    Given int array, determine min jumps to reach last index
-    Ex. nums = [2,3,1,1,4] -> 2, index 0 to 1 to last
-
-    Greedy: At each point, determine furthest reachable, jump to it
-
-    Time: O(n)
-    Space: O(1)
-*/
-
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        int result = 0;
-        
-        int i = 0;
-        while (i < n - 1) {
-            if (i + nums[i] >= n - 1) {
-                result++;
-                break;
+        int n=nums.size();
+        int l=0,r=0,ans=0,farthest=0;
+        while(r<n-1){
+            farthest=0;
+            for(int i=l;i<=r;i++){
+                farthest=max(farthest,i+nums[i]);
             }
-            int maxIndex = i + 1;
-            int maxValue = 0;
-            for (int j = i + 1; j < i + 1 + nums[i]; j++) {
-                if (j + nums[j] > maxValue) {
-                    maxIndex = j;
-                    maxValue = j + nums[j];
-                }
-            }
-            i = maxIndex;
-            result++;
+            l=r+1;
+            r=farthest;
+            ans++;
         }
-        
-        return result;
+        return ans;
     }
 };
