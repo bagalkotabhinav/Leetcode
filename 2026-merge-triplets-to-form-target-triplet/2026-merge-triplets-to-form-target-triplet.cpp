39518dd2fed0333b5fd9c0974d56f1cfaac6b3c3
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-06-05 15:38:50 +0530

    Time: 354 ms (56.59%), Space: 150 MB (48.93%) - LeetHub

--- 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    bool mergeTriplets(vector<vector<int>>& a, vector<int>& b) {
-        int n=a.size();
-        unordered_set<int> k;
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        int n=triplets.size();
+        unordered_set<int> num;
         for(int i=0;i<n;i++){
-            if(a[i][0]>b[0] || a[i][1]>b[1] || a[i][2]>b[2])
+            if(triplets[i][0]>target[0] || triplets[i][1]>target[1] || triplets[i][2]>target[2])
                 continue;
             for(int j=0;j<3;j++){
-                if(a[i][j]==b[j])
-                    k.insert(j);
+                if(triplets[i][j]==target[j])
+                    num.insert(j);
             }
         }
-        return k.size()==3;
+        return num.size()==3;
     }
 };
