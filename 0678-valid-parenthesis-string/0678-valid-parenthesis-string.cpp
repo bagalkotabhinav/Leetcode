ea8c592554665beb91faf6f71cd27748cbd726a7
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-06-09 10:36:12 +0530

    Time: 0 ms (100%), Space: 7.1 MB (92.47%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int n = s.size();
-    
-        int balanced = 0;
-        for(int i=0; i<n; i++) {
-            if(s[i] == '(' || s[i] == '*') 
-                balanced++;
-            else 
-                balanced--;
-
-            if(balanced < 0) 
+        int leftmin=0,leftmax=0;
+        for(auto i: s){
+            if(i=='('){
+                leftmin++;
+                leftmax++;
+            }
+            else if(i==')'){
+                leftmin--;
+                leftmax--;
+            }
+            else{
+                leftmin--;
+                leftmax++;
+            }
+            if(leftmax<0)
                 return false;
-        }
-
-        if(balanced == 0) 
-            return true;
-
-        balanced = 0;
-        for(int i=n-1; i>=0; i--) {
-            if(s[i] == ')' || s[i] == '*') 
-                balanced++;
-            else 
-                balanced--;
-
-            if(balanced < 0) 
-                return false;
-        }
-
-        return true;
+            if(leftmin<0)
+                leftmin=0;
+        } 
+        return leftmin==0;
     }
 };
