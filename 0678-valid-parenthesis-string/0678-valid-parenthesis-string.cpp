bf237cc720318155c564609f791bc2e5e294cd42
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-06-09 10:33:55 +0530

    Time: 0 ms (100%), Space: 7.1 MB (92.47%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftmin=0,leftmax=0;
+        for(auto i: s){
+            if(i=='('){
+                leftmin++;
+                leftmax++;
+            }
+            else if(i==')'){
+                leftmin--;
+                leftmax--;
+            }
+            else{
+                leftmin--;
+                leftmax++;
+            }
+            if(leftmax<0)
+                return false;
+            if(leftmin<0)
+                leftmin=0;
+        } 
+        return leftmin==0;
+    }
+};
