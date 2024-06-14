82202a448843a0645661deaf4bd271a8bed4c0ec
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-06-14 16:01:37 +0530

    Time: 0 ms (100%), Space: 7.7 MB (45.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -3,24 +3,16 @@ public:
     int uniquePaths(int m, int n) {
         if(m==1 && n==1)
             return 1;
-        vector<vector<int>> adjmat(m,vector<int>(n,-1));
-        adjmat[m-1][n-1]=0;
-        for(int i=m-2;i>=0;i--)
+        vector<vector<int>> adjmat(m,vector<int>(n,0));
+        for(int i=0;i<m-1;i++)
             adjmat[i][n-1]=1;
-        for(int i=n-2;i>=0;i--)
+        for(int i=0;i<n-1;i++)
             adjmat[m-1][i]=1;
-        
         for(int i=m-2;i>=0;i--){
             for(int j=n-2;j>=0;j--){
                 adjmat[i][j]=adjmat[i+1][j]+adjmat[i][j+1];
             }
         }
-        // cout<<adjmat[0][0]<<endl;
         return adjmat[0][0];
     }
 };
-
-//can go right
-//can go down
-//recursion
-//edge cases
