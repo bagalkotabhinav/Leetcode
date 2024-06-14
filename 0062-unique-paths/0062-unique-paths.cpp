927a37af88a270061a7292a7f233a95f4daa9593
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-06-14 17:47:07 +0530

    Time: 0 ms (100%), Space: 7.6 MB (45.03%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        if(m==1 && n==1)
-            return 1;
         vector<vector<int>> adjmat(m,vector<int>(n,0));
-        for(int i=0;i<m-1;i++)
-            adjmat[i][n-1]=1;
-        for(int i=0;i<n-1;i++)
-            adjmat[m-1][i]=1;
-        for(int i=m-2;i>=0;i--){
-            for(int j=n-2;j>=0;j--){
-                adjmat[i][j]=adjmat[i+1][j]+adjmat[i][j+1];
+        for(int i=0;i<n;i++)
+            adjmat[0][i]=1;
+        for(int i=0;i<m;i++)
+            adjmat[i][0]=1;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                adjmat[i][j]=adjmat[i-1][j]+adjmat[i][j-1];
             }
         }
-        return adjmat[0][0];
+        return adjmat[m-1][n-1];
     }
 };
