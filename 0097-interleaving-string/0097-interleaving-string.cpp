fb82c7a2b6e087f9b2e3e699889ece7f5103b738
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-06-26 13:30:32 +0530

    Time: 24 ms (28.91%), Space: 33.7 MB (23.76%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
-
-    bool solve(string &s1, string &s2, string &s3, int i ,int j, int index, vector<vector<int>> &dp){
-        if(index==s3.size())
+    bool ans(string s1, string s2, string s3, int i, int j, int k, vector<vector<int>>& dp){
+        if(k==s3.size())
             return true;
         if(dp[i][j]!=-1)
             return dp[i][j];
-
-        bool a = false;
-        if(s1[i]==s3[index] && i<s1.size()){
-            a = solve(s1,s2,s3,i+1,j,index+1,dp);
-        }
-
-        bool b = false;
-        if(s2[j]==s3[index] && j<s2.size()){
-            b = solve(s1,s2,s3,i,j+1,index+1,dp);
-        }
-
-        dp[i][j] = a || b;
-        return dp[i][j];
         
-    }
+        bool a=false;
+        if(i<s1.size() && s1[i]==s3[k])
+            a=ans(s1,s2,s3,i+1,j,k+1,dp);
+        
+        bool b=false;
+        if(j<s2.size() && s2[j]==s3[k])
+            b=ans(s1,s2,s3,i,j+1,k+1,dp);
+        
+        dp[i][j]=a || b;
 
+        return dp[i][j];        
+    }
     bool isInterleave(string s1, string s2, string s3) {
-        int n=s1.size();
-        int m=s2.size();
-        if(s3.size()!=n+m)
+        if(s1.size()+s2.size()!=s3.size())
             return false;
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return solve(s1,s2,s3,0,0,0,dp);
+        vector<vector<int>> dp(s1.size()+1,vector<int>(s2.size()+1,-1));
+        return ans(s1,s2,s3,0,0,0,dp);
     }
 };
