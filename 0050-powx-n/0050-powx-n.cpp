e296d393067c837147c26739a143c17afe9fe4a0
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-07 11:46:12 +0530

    Time: 0 ms (100%), Space: 8.1 MB (45.12%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x,n);
+        if(n<0)
+            x=1/x;
+        double pow=1;
+        long num=labs(n);
+        while(num){
+            if(num&1)
+                pow=pow*x;
+            x=x*x;
+            num=num>>1;
+        }
+        return pow;
     }
 };
+
+// x=7 n=11
+// 11=(1011)2 == 8+2+1
+// 7^11= 7^8 + 7^2 + 7^1
+// x=7
+// pow*=x iff n&1==1
+// x*=x
