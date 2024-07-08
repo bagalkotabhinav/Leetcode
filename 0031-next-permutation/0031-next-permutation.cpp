15fbfd4c5af760088bfca3a754cbdc110714c84f
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-08 10:26:17 +0530

    Time: 0 ms (100%), Space: 14.6 MB (15.9%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int i,j;
+
+        //FIND JUST DECREASING ELEMENT FROM RIGHT
         for(i=nums.size()-2;i>=0;i--){
             if(nums[i]<nums[i+1])
                 break;
         }
+        //LOOP TO FIRST ORDER
         if(i<0)
             reverse(nums.begin(),nums.end());
+
         else{
+            //FIND JUST INCREASING ELEMENT FROM i
             for(j=nums.size()-1;j>=0;j--){
                 if(nums[j]>nums[i])
                     break;
             }
+            //LEXICOGRAPHICAL ORDER
             swap(nums[i],nums[j]);
             reverse(nums.begin()+i+1,nums.end());
         }
