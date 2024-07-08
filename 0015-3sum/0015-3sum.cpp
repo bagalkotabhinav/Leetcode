ac6c2299d0e7efc331522d4b9ecb97884682ac04
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-08 10:45:00 +0530

    Time: 66 ms (55.48%), Space: 27.3 MB (57.65%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,79 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        ios_base::sync_with_stdio(false); cin.tie(NULL);
         sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
-
-        for(int i=0;i<nums.size()-2;i++){
+        int i,j,k;
+        for(int i=0;i<=nums.size()-3;i++){
             if(i>0 && nums[i]==nums[i-1])
                 continue;
-            int j=i+1,k=nums.size()-1;
-
+            j=i+1,k=nums.size()-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
                 if(sum==0){
                     ans.push_back({nums[i],nums[j],nums[k]});
-
-                    while(j<k && nums[j+1]==nums[j]){
+                    while(j<k && nums[j]==nums[j+1])
                         j++;
-                    }
-                    while(j<k && nums[k-1]==nums[k]){
+                    j++;
+                    while(j<k && nums[k]==nums[k-1])
                         k--;
-                    }
-                    j++;
                     k--;
                 }
-                else if(sum>0){
+                else if(sum>0)
                     k--;
-                }
-                else{
+                else
                     j++;
-                }
             }
         }
         return ans;
     }
 };
-
-
-
-
-
-
-// class Solution {
-// public:
-//     vector<vector<int>> threeSum(vector<int>& nums) {
-//         vector<vector<int>> ans;
-//         set<vector<int>> a1;
-//         sort(nums.begin(),nums.end());
-//         for(int i=0;i<nums.size();i++){
-//             int temp=i;
-//             int j=i+1,k=nums.size()-1;
-//             vector<int> a;
-//             while(j<k){
-//                 if((nums[i]+nums[j]+nums[k])==0){
-//                     a.push_back(nums[i]);
-//                     a.push_back(nums[j]);
-//                     a.push_back(nums[k]);
-//                     ans.push_back(a);
-//                     a1.insert(a);
-//                     a.clear();
-                    
-//                     i=temp;
-//                     j++;
-//                     k--;
-                
-//                 }
-//                 else if((nums[i]+nums[j]+nums[k])<0){
-//                     j++;
-//                 }
-//                 else if((nums[i]+nums[j]+nums[k])>0){
-//                     k--;
-//                 }
-//             }
-//         }
-//        vector<vector<int>> a2(a1.begin(),a1.end());
-//         return a2;
-//     }
-// };
