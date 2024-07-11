3728475220d5ea6edf97918a7b007e7597aa6e16
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-11 17:43:07 +0530

    Time: 43 ms (12.29%), Space: 21.7 MB (16.47%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -10,14 +10,17 @@ public:
     int minEatingSpeed(vector<int>& piles, int h) {
         long long int low=1,high=*max_element(piles.begin(),piles.end());
         long long int mid;
+        long long int ans=INT_MAX;
         while(low<=high){
             mid=(low+high)/2;
             long long int time=findtime(piles,mid);
-            if(time<=h)
+            if(time<=h){
                 high=mid-1;
+                ans=min(ans,mid);
+            }
             else
                 low=mid+1;
         }
-        return low;
+        return ans;
     }
 };
