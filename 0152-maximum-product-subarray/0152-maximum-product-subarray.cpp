8da75d5e54f4ccc3094a689a180e1c52408c6e49
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-11 11:01:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int max_prod=1,min_prod=1,ans=nums[0];
+        int maxi=1,mini=1,ans=nums[0];
         for(auto i: nums){
-            int max_temp=max_prod*i;
-            int min_temp=min_prod*i;
-            int curmax=max(max_temp,min_temp);
-            int curmin=min(max_temp,min_temp);
-            max_prod=max(curmax,i);
-            min_prod=min(curmin,i);
-            ans=max(ans,max_prod);
+            int maxim=maxi*i;
+            int minim=mini*i;
+            int curmax=max(maxim,minim);
+            int curmin=min(maxim,minim);
+            maxi=max(curmax,i);
+            mini=min(curmin,i);
+            ans=max(ans,maxi);
         }
         return ans;
     }
 };
-
-// max_prod: stores max of: all elements till current index and current number
-// min_prod: stores min of: all elements till current index and current number
-// during each iteration, store maximum
