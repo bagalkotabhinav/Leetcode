310dd2f670aafc64f5bd3a59f5531a5d4d759a4e
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-13 17:01:59 +0530

    Time: 8 ms (59.65%), Space: 10.9 MB (18.11%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -6,32 +6,19 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- auto init = []() {
-    cin.sync_with_stdio( false );
-    fstream out( "user.out" );
-    for ( string s; getline( cin, s ); ) {
-        getline( cin, s );
-        s[0] != '-' ? out << "true\n" : out << "false\n";
-    }
-    out.flush();
-    exit(0);
-    return 0;
-}();
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode * fast=head;
         ListNode * slow=head;
-
-        while(fast!=NULL && fast->next!=NULL){
+        ListNode * fast=head;
+        if(head==NULL)
+            return false;
+        while(fast!=NULL && fast->next !=NULL){
             slow=slow->next;
             fast=fast->next->next;
-
-            if(fast==slow)
+            if(slow==fast)
                 return true;
         }
-        
         return false;
     }
 };
