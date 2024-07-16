27cb5a3c4345b3a8b6dd4bca8dbc26aaf0bdcc4c
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-16 11:34:32 +0530

    Time: 192 ms (91.1%), Space: 9.9 MB (73.46%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    bool solve(int i, int j, vector<vector<char>>& board, int k, string& word){
+    bool solve(int i, int j, vector<vector<char>>& board, string& word, int k){
         if(k>=word.size())
             return true;
-        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!=word[k])
+        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j]!=word[k])
             return false;
-
         char c=board[i][j];
         board[i][j]='#';
-        bool top=solve(i-1,j,board,k+1,word);
-        bool bottom=solve(i+1,j,board,k+1,word);
-        bool right=solve(i,j+1,board,k+1,word);
-        bool left=solve(i,j-1,board,k+1,word);
+        bool top=solve(i-1,j,board,word,k+1);
+        bool down=solve(i+1,j,board,word,k+1);
+        bool left=solve(i,j-1,board,word,k+1);
+        bool right=solve(i,j+1,board,word,k+1);
         board[i][j]=c;
-        return top||bottom||left||right;
+        return top||down||left||right;
     }
     bool exist(vector<vector<char>>& board, string word) {
         for(int i=0;i<board.size();i++){
             for(int j=0;j<board[0].size();j++){
-                if(solve(i,j,board,0,word))
+                if(solve(i,j,board,word,0))
                     return true;
             }
         }
