8c758b311c9b433d3f69d64ec19b134ebae80178
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-16 11:29:43 +0530

    Time: 200 ms (86.39%), Space: 9.8 MB (76.28%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    bool solve(const int& m, const int& n, vector<vector<char>>& board, int i, int j, string& word, int k){
-        if(k==word.size())
+    bool solve(int i, int j, vector<vector<char>>& board, int k, string& word){
+        if(k>=word.size())
             return true;
-        if(i==m || j==n || i<0 || j<0 || board[i][j]!=word[k])
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!=word[k])
             return false;
-        
+
         char c=board[i][j];
-        board[i][j]='.';
-
-        bool top=solve(m,n,board,i-1,j,word,k+1);
-        bool down=solve(m,n,board,i+1,j,word,k+1);
-        bool left=solve(m,n,board,i,j-1,word,k+1);
-        bool right=solve(m,n,board,i,j+1,word,k+1);
-
+        board[i][j]='#';
+        bool top=solve(i-1,j,board,k+1,word);
+        bool bottom=solve(i+1,j,board,k+1,word);
+        bool right=solve(i,j+1,board,k+1,word);
+        bool left=solve(i,j-1,board,k+1,word);
         board[i][j]=c;
-
-        return top||down||left||right;
-
+        return top||bottom||left||right;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(solve(m,n,board,i,j,word,0))
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(solve(i,j,board,0,word))
                     return true;
             }
         }
         return false;
     }
 };
-
-
-// index k to map word's current character to current word in board (board[i][j])
-// if it is not equal to word[k], it means cannot get word from current cell, return false
-// if it is equal, check for all positions: up, down, left, right
-// if any one of them returns true, then final word can be reached with current cell (board[i][j])
