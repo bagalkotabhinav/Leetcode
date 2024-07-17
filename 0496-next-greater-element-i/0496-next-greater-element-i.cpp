fa3ea8652bfff21af2b129af541725bcb56b206a
Abhinav Bagalkot <140387230+bagalkotabhinav@users.noreply.github.com>
2024-07-17 11:56:51 +0530

    Time: 4 ms (70.14%), Space: 12.5 MB (6.32%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,26 +3,18 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> st;
         unordered_map<int,int> mp;
+        vector<int>ans;
         for(int i=nums2.size()-1;i>=0;i--){
             while(!st.empty() && nums2[i]>st.top())
                 st.pop();
-
             if(st.empty())
                 mp.insert({nums2[i],-1});
-            
             else
                 mp.insert({nums2[i],st.top()});
-            
             st.push(nums2[i]);
         }
-        vector<int> ans;
         for(auto i: nums1)
             ans.push_back(mp[i]);
         return ans;
     }
 };
-
-// piche se keep pushing elements in stack
-// at first push last ele, and assign -1
-// if cur ele in nums2>stack top, pop all less elements and push current element
-// else if cur ele in nums2<stack top, push and set nextg as stack top
